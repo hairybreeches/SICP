6531700e9924b63e7fa2b0f7d6101721b1153564
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2015-05-15 17:59:30 +0100

    exercise 1.31: make product iterative

--- src/sicp/1_31.clj
+++ src/sicp/1_31.clj
@@ -1,9 +1,12 @@
 (ns sicp.1-31)
 
 (defn product [term start iterator finish]
-  (if (> start finish)
-      1
-      (* (term start) (product term (iterator start) iterator finish))))
+  (defn iter[start result]
+    (if (> start finish)
+      result
+      (iter (iterator start) (* (term start) result))))
+  (iter start 1))
+
 
 (defn factorial [n]
   (product identity 1 inc n))
