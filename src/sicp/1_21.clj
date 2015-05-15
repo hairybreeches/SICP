5f37ff0634cc97146d19f0fcdc4eee546604a07d
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2015-05-15 11:08:35 +0100

    rename function

--- src/sicp/1_21.clj
+++ src/sicp/1_21.clj
@@ -4,7 +4,7 @@
 (defn divides? [a b]
   (= (rem b a) 0))
 
-(defn next[n]
+(defn next-divisor [n]
   (if (= n 2) 3 (+ n 2)))
 
 
@@ -14,7 +14,7 @@
          test-divisor test-divisor]
     (cond (> (square test-divisor) n) n
           (divides? test-divisor n) test-divisor
-          :else (recur n (next test-divisor)))))
+          :else (recur n (next-divisor test-divisor)))))
 
 (defn smallest-divisor [n]
   (find-divisor n 2))
