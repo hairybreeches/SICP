bb566d2e3776b5e860d13ab30cfab090a99289ba
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2015-05-15 10:37:49 +0100

    exercise 1.23

--- src/sicp/1_21.clj
+++ src/sicp/1_21.clj
@@ -4,13 +4,17 @@
 (defn divides? [a b]
   (= (rem b a) 0))
 
+(defn next[n]
+  (if (= n 2) 3 (+ n 2)))
+
+
 
 (defn find-divisor [n test-divisor]
   (loop [n n
          test-divisor test-divisor]
     (cond (> (square test-divisor) n) n
           (divides? test-divisor n) test-divisor
-          :else (recur n (+ test-divisor 1)))))
+          :else (recur n (next test-divisor)))))
 
 (defn smallest-divisor [n]
   (find-divisor n 2))
