9dbb371b5a69ef6af505ef8286da2cd5ac666eb8
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2015-05-15 10:36:31 +0100

    update samples for better scaling

--- src/sicp/1_21.clj
+++ src/sicp/1_21.clj
@@ -37,10 +37,18 @@
   (time (report-if-prime n)))
 
 (defn show-prime-complexity []
-  (timed-prime-test 1009)
-  (timed-prime-test 10007)
-  (timed-prime-test 100003)
-  (timed-prime-test 1000003))
+  (timed-prime-test 1000000007)
+  (timed-prime-test 1000000009)
+  (timed-prime-test 1000000021)
+  (timed-prime-test 10000000019)
+  (timed-prime-test 10000000033)
+  (timed-prime-test 10000000061)
+  (timed-prime-test 100000000003)
+  (timed-prime-test 100000000019)
+  (timed-prime-test 100000000057)
+  (timed-prime-test 1000000000039)
+  (timed-prime-test 1000000000061)
+  (timed-prime-test 1000000000063))
 
 
 
