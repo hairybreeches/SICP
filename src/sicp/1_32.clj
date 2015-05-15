c208b786b037ad8b13683b059a9c062c1b874bba
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2015-05-15 18:49:23 +0100

    add filtered accumulate

--- src/sicp/1_32.clj
+++ src/sicp/1_32.clj
@@ -1,17 +1,18 @@
 (ns sicp.1-32)
 
-(defn accumulate-recursive [combiner null-value term start iterator finish]
-    (if (> start finish)
-      null-value
-      (combiner (term start) (accumulate-recursive combiner null-value term (iterator start)  iterator finish))))
-
-
-
-(defn accumulate [combiner null-value term start iterator finish]
+(defn filtered-accumulate [predicate combiner null-value term start iterator finish]
   (loop [start start
          result null-value]
     (if (> start finish)
       result
-      (recur (iterator start) (combiner (term start) result)))))
+      (recur
+         (iterator start)
+         (let [this-term (term start)]
+           (if (predicate this-term)
+               (combiner this-term result)
+               result))))))
+
+(defn accumulate [combiner null-value term start iterator finish]
+  (filtered-accumulate (fn [x] true) combiner null-value term start iterator finish))
 
 
