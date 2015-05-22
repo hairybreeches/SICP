4e175b049a31858b4d33c279b4d6308cd880f1e2
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2015-05-22 15:32:36 +0100

    ex 1.38: calculate e with continued fractions

--- test/sicp/continued_fraction_tests.clj
+++ test/sicp/continued_fraction_tests.clj
@@ -14,3 +14,20 @@
    (approximate-reciprocal-phi 10)
    reciprocal-phi
    4))
+
+(defn euler-denominator [n]
+  (if (= (rem n 3) 2)
+    (* 2 (/ (+ 1 n) 3))
+    1))
+
+(deftest test-euler-denominator
+  (is (= (map euler-denominator (range 1 15)) [1 2 1 1 4 1 1 6 1 1 8 1 1 10])))
+
+(defn approximate-e [number-of-terms]
+  (+ 2 (cont-frac (constantly 1) euler-denominator number-of-terms)))
+
+(def e 2.718281828459045)
+
+(deftest calculate-e
+  (is-roughly= (approximate-e 15) e 10))
+
