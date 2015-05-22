e89cfddc87d2968cd9da09d7acd05531b8fe73bf
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2015-05-22 14:36:23 +0100

    Backed out changeset: 1efb5975404b

--- src/sicp/1_35.clj
+++ src/sicp/1_35.clj
@@ -5,7 +5,6 @@
   (defn close-enough? [v1 v2]
     (< (abs (- v1 v2)) tolerance))
   (loop [guess first-guess]
-    (prn guess)
     (let [next-guess (f guess)]
       (if (close-enough? guess next-guess)
         next-guess
--- src/sicp/core.clj
+++ src/sicp/core.clj
@@ -1,14 +1,8 @@
 (ns sicp.core
-  (:use sicp.1-35))
-
-(defn function [x]
-  (/ (java.lang.Math/log 1000) (java.lang.Math/log x)))
+  (:use sicp.1-22))
 
 
 (defn -main
   "steve"
   []
-  (fixed-point function 2 0.00001)
-  (prn)
-  (fixed-point #(/ (+ (function %) %) 2) 2 0.0001))
-
+  (show-prime-complexity))
