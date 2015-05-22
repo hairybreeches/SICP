e4728a40516318278d653761b05b254358fe838e
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2015-05-22 14:33:42 +0100

    code for exercise 1.36

--- src/sicp/1_35.clj
+++ src/sicp/1_35.clj
@@ -5,6 +5,7 @@
   (defn close-enough? [v1 v2]
     (< (abs (- v1 v2)) tolerance))
   (loop [guess first-guess]
+    (prn guess)
     (let [next-guess (f guess)]
       (if (close-enough? guess next-guess)
         next-guess
--- src/sicp/core.clj
+++ src/sicp/core.clj
@@ -1,8 +1,14 @@
 (ns sicp.core
-  (:use sicp.1-22))
+  (:use sicp.1-35))
+
+(defn function [x]
+  (/ (java.lang.Math/log 1000) (java.lang.Math/log x)))
 
 
 (defn -main
   "steve"
   []
-  (show-prime-complexity))
+  (fixed-point function 2 0.00001)
+  (prn)
+  (fixed-point #(/ (+ (function %) %) 2) 2 0.0001))
+
