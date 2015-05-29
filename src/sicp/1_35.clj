3a5dd6359d1eaf3ea97bb47a4fb9ab55f870c898
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2015-05-29 16:27:29 +0100

    ex 1.46

--- src/sicp/1_35.clj
+++ src/sicp/1_35.clj
@@ -1,12 +1,9 @@
 (ns sicp.1-35
+  (:use sicp.1-46)
   (:use clojure.math.numeric-tower))
 
 (defn fixed-point [f first-guess tolerance]
-  (defn close-enough? [v1 v2]
-    (< (abs (- v1 v2)) tolerance))
-  (loop [guess first-guess]
-    (let [next-guess (f guess)]
-      (if (close-enough? guess next-guess)
-        next-guess
-        (recur next-guess)))))
+  (defn close-enough? [guess next-guess]
+    (< (abs (- guess next-guess)) tolerance))
+  ((iterative-improve close-enough? f) first-guess))
 
