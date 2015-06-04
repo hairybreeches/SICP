0ad0f03671c71b23b676487bb57d0e966fc96e5a
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2015-06-04 19:26:11 +0100

    be explicit we're getting the numerator and denominator right and tests for 2.1

--- test/sicp/rational_numbers_test.clj
+++ test/sicp/rational_numbers_test.clj
@@ -2,19 +2,30 @@
   (:use clojure.test)
   (:use sicp.chapter-2.rational-numbers))
 
-(defn is-rat-equal [rat x]
-  (is (= (/ (numer rat) (denom rat)) x)))
+(defn is-rat-equal [rat n d]
+  (is (= (numer rat) n))
+  (is (= (denom rat)) d))
 
 
 (deftest should-be-able-to-add
-  (is-rat-equal (add-rat (make-rat 1 3) (make-rat 1 3)) 2/3))
+  (is-rat-equal (add-rat (make-rat 1 3) (make-rat 1 3)) 2 3))
 
 (deftest should-be-able-to-subtract
-  (is-rat-equal (sub-rat (make-rat 2 3) (make-rat 1 2)) 1/6))
+  (is-rat-equal (sub-rat (make-rat 2 3) (make-rat 1 2)) 1 6))
 
 (deftest should-be-able-to-multiply
-  (is-rat-equal (mul-rat (make-rat 2 3) (make-rat 3 4)) 1/2))
+  (is-rat-equal (mul-rat (make-rat 2 3) (make-rat 3 4)) 1 2))
 
 (deftest should-be-able-to-divide
-  (is-rat-equal (div-rat (make-rat 2 3) (make-rat 4 3)) 1/2))
+  (is-rat-equal (div-rat (make-rat 2 3) (make-rat 4 3)) 1 2))
+
+
+(deftest if-denominator-negative-and-numerator-positive
+  (is-rat-equal (make-rat 2 -3) -2 3))
+
+(deftest if-numerator-and-denominator-both-negative
+  (is-rat-equal (make-rat -2 -6) 1 3))
+
+(deftest if-numerator-and-denominator-both-positive
+  (is-rat-equal (make-rat 7 14) 1 2))
 
