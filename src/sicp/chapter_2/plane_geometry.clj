5960cb31f7322d242e49c7054c2b707e6deb0896
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2015-06-05 15:30:16 +0100

    exercise 2.3, first implementation

--- src/sicp/chapter_2/plane_geometry.clj
+++ src/sicp/chapter_2/plane_geometry.clj
@@ -1,5 +1,6 @@
 (ns sicp.chapter-2.plane-geometry
-  (:use sicp.average))
+  (:use sicp.average)
+  (:use clojure.math.numeric-tower))
 
 (defn make-segment [start end]
   [start end])
@@ -19,6 +20,14 @@
 (defn y-point [point]
   (second point))
 
+(defn distance[point1 point2]
+  (sqrt (+ (expt (- (x-point point1)
+                    (x-point point2))
+                 2)
+           (expt (- (y-point point1)
+                    (y-point point2))
+                 2))))
+
 (defn point-average [point1 point2]
   (make-point (average (x-point point1) (x-point point2))
               (average (y-point point1) (y-point point2))))
@@ -26,6 +35,40 @@
 (defn midpoint [segment]
   (point-average (start-segment segment)
                  (end-segment segment)))
+
+
+(defn make-rectangle [point1 point2 point3 point4]
+  [point1 point2 point3 point4])
+
+(defn point1 [rectangle]
+  (first rectangle))
+
+(defn point2 [rectangle]
+  (second rectangle))
+
+(defn point3 [rectangle]
+  (nth rectangle 2))
+
+(defn point4 [rectangle]
+  (nth rectangle 3))
+
+(defn width [rectangle]
+  (distance (point1 rectangle)
+            (point2 rectangle)))
+
+(defn height [rectangle]
+  (distance (point2 rectangle)
+            (point3 rectangle)))
+
+(defn area [rectangle]
+  (* (width rectangle)
+     (height rectangle)))
+
+
+(defn perimeter [rectangle]
+  (* 2
+    (+ (width rectangle)
+       (height rectangle))))
 
 
 
