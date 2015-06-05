d7fb6c6260ee0bd7571eef9bc422a5a842d27c19
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2015-06-05 17:36:39 +0100

    code from the book and tests for add, multiply, ex 2.7

---
+++ src/sicp/chapter_2/interval_arithmetic.clj
@@ -0,0 +1,29 @@
+(ns sicp.chapter-2.interval-arithmetic)
+
+(defn make-interval [lower upper]
+  [lower upper])
+
+(defn upper-bound [interval]
+  (second interval))
+
+(defn lower-bound [interval]
+  (first interval))
+
+(defn add-interval [a b]
+  (make-interval (+ (lower-bound a)
+                    (lower-bound b))
+                 (+ (upper-bound a)
+                    (upper-bound b))))
+
+(defn mul-interval [a b]
+  (let [attempts [(* (upper-bound a) (upper-bound b))
+                  (* (upper-bound a) (lower-bound b))
+                  (* (lower-bound a) (upper-bound b))
+                  (* (lower-bound a) (lower-bound b))]
+        ]
+    (make-interval (apply min attempts)
+                   (apply max attempts))))
+
+(defn div-interval [a b]
+  (mul-interval a (make-interval (/ 1 (upper-bound b))
+                                 (/ 1 (lower-bound a)))))
---
+++ test/sicp/interval_arithmetic_test.clj
@@ -0,0 +1,67 @@
+(ns sicp.interval-arithmetic-test
+  (:use sicp.chapter-2.interval-arithmetic)
+  (:use clojure.test))
+
+(defn is-interval-equal [interval lower upper]
+  (is (= (lower-bound interval) lower))
+  (is (= (upper-bound interval)) upper))
+
+(deftest add-ranges-test
+  (is-interval-equal
+   (add-interval
+    (make-interval 1 4)
+    (make-interval 2 6))
+   3
+   10))
+
+(deftest mul-ranges-test-both-positive
+  (is-interval-equal
+   (mul-interval
+    (make-interval 1 4)
+    (make-interval 2 6))
+   2
+   24))
+
+(deftest mul-ranges-test-both-negative
+  (is-interval-equal
+   (mul-interval
+    (make-interval -1 -4)
+    (make-interval -2 -6))
+   2
+   24))
+
+(deftest mul-ranges-test-one-negative-one-positive
+  (is-interval-equal
+   (mul-interval
+    (make-interval -1 -4)
+    (make-interval 2 6))
+   -24
+   -2))
+
+(deftest mul-ranges-test-one-positive-one-spans
+  (is-interval-equal
+   (mul-interval
+    (make-interval -1 4)
+    (make-interval 2 6))
+   -6
+   24))
+
+(deftest mul-ranges-test-one-negative-one-spans
+  (is-interval-equal
+   (mul-interval
+    (make-interval -1 4)
+    (make-interval -2 -6))
+   -24
+   6))
+
+(deftest mul-ranges-test-both-span
+  (is-interval-equal
+   (mul-interval
+    (make-interval -1 4)
+    (make-interval -2 6))
+   -8
+   24))
+
+
+
+
