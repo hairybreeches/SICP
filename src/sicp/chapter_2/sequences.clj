0b154149992ec9e0e271b5195797c03d980e4c38
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2015-06-12 18:05:43 +0100

    exercise 2.18: reversing a sequence

--- src/sicp/chapter_2/sequences.clj
+++ src/sicp/chapter_2/sequences.clj
@@ -7,3 +7,10 @@
         (first things)
         (recur cdr)))))
 
+(defn reverse-impl[things]
+  (loop [so-far '()
+         things things]
+    (if (empty? things)
+        so-far
+        (recur (cons (first things) so-far) (rest things)))))
+
--- test/sicp/test/chapter_2/sequences.clj
+++ test/sicp/test/chapter_2/sequences.clj
@@ -3,8 +3,18 @@
   (:use clojure.test))
 
 
-(deftest last-item--in-single-element-sequence
+(deftest last-item-in-single-element-sequence
   (is (= :a (last-pair [:a]))))
 
-(deftest last-item--in-several-element-sequence
-  (is (= 6 (last-pair '(1 2 3 4 6)))))
+(deftest last-item-in-several-element-sequence
+  (is (= 6 (last-pair '(1 2 3 4 5 6)))))
+
+(deftest reverse-empty-sequence
+  (is (empty? (reverse-impl '()))))
+
+(deftest reverse-single-element-sequence
+  (is (= '(:zi) (reverse-impl '(:zi)))))
+
+(deftest reverse-several-element-sequence
+  (is (= '(6 5 4 3 2 1) (reverse-impl '(1 2 3 4 5 6)))))
+
