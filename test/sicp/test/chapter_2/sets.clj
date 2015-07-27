88243135649b26236e0e73348d847f6e6bb34fd6
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2015-07-27 21:16:20 +0100

    parametrise tests

--- test/sicp/test/chapter_2/sets.clj
+++ test/sicp/test/chapter_2/sets.clj
@@ -3,17 +3,22 @@
   (:use clojure.test))
 
 (deftest element-of-set-true
-  (is (ul-element-of-set? 2 (ul-make-set 4 6 2))))
+  (are [element-of-set? make-set] (element-of-set? 2 (make-set 4 6 2))
+       ul-element-of-set? ul-make-set))
 
 (deftest element-of-set-false
-  (is (not (ul-element-of-set? 2 (ul-make-set 4 6 3)))))
+  (are [element-of-set? make-set] (not (element-of-set? 2 (make-set 4 6 3)))
+       ul-element-of-set? ul-make-set))
 
-(deftest adjoin-set
-  (is (ul-set= (ul-adjoin-set 1 (ul-make-set 2 3 4)) (ul-make-set 1 2 3 4))))
+(deftest adjoin
+  (are [set= adjoin-set make-set] (set= (adjoin-set 1 (make-set 2 3 4)) (make-set 1 2 3 4))
+      ul-set= ul-adjoin-set ul-make-set))
 
-(deftest intersection-set
-  (is (ul-set= (ul-intersection-set (ul-make-set 2 3 4) (ul-make-set 5 4 3)) (ul-make-set 3 4))))
+(deftest intersection
+  (are [set= intersection-set make-set] (set= (intersection-set (make-set 2 3 4) (make-set 5 4 3)) (make-set 3 4))
+       ul-set= ul-intersection-set ul-make-set))
 
 
-(deftest union-set
-  (is (ul-set= (ul-union-set (ul-make-set 1 5 4) (ul-make-set  3 2 4 6)) (apply ul-make-set (range 1 7)))))
+(deftest union
+  (are [set= union-set make-set] (set= (union-set (make-set 1 5 4) (make-set  3 2 4 6)) (apply make-set (range 1 7)))
+       ul-set= ul-union-set ul-make-set))
