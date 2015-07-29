8a9526497ec4fb0b2782e2909f905269458fc340
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2015-07-29 21:48:08 +0100

    exercise 2.61 (kinda) and 2.62 - sets as ordered lists.

--- src/sicp/chapter_2/sets.clj
+++ src/sicp/chapter_2/sets.clj
@@ -30,6 +30,59 @@
 (defn ul-set=[set1 set2]
   (= (sort set1) (sort set2)))
 
+;sets as orderered lists
+(defn ol-element-of-set?[x a-set]
+  (loop [x x
+         a-set a-set]
+    (cond (empty? a-set) false
+          (= x (first a-set)) true
+          (< x (first a-set)) false
+          :else (recur x (rest a-set)))))
+
+;note that this isn't really "half" the number of steps of the unordered version,
+;since the append just involves slamming the new element on the front. so the worst case is traversing the list twice.
+;it's still order n, but you need to parse the "before" elements three times (once on the stack, once for the reverse and once for the concat)
+;the version on the solutions wiki (http://community.schemewiki.org/?sicp-ex-2.61) only goes through the "before" stack once
+;since you avoid the reverse + concat by queuing the appends up as part of one "step".
+;I'm pretty sure it's a bit more efficient in terms of operations
+;(although obviously no tail recursion makes it a moot point since large sets will crash the stack)
+;but the slight complication about what precisely constitutes a "step" kinda shows why big-O notation is a useful tool.
+(defn ol-adjoin-set[x a-set]
+  (loop [before '()
+         after a-set]
+    (let [current (first after)]
+      (cond (= x current) a-set
+            (> x current) (recur (cons current before) (rest after))
+            (< x current) (concat (reverse (cons x before)) after)))))
+
+(defn ol-intersection-set[set1 set2]
+  (if (or (empty? set1) (empty? set2))
+      '()
+      (let [x1 (first set1)
+            x2 (first set2)]
+          (cond (= x1 x2) (cons x1 (ol-intersection-set (rest set1) (rest set2)))
+                (< x1 x2) (ol-intersection-set (rest set1) set2)
+                (> x1 x2) (ol-intersection-set set1 (rest set2))))))
+
+(defn ol-union-set[set1 set2]
+  (reverse
+    (loop [set1 set1
+           set2 set2
+           result '()]
+      (cond (empty? set1) (concat (reverse set2) result)
+            (empty? set2) (concat (reverse set1) result)
+            :else (let [x1 (first set1)
+                        x2 (first set2)]
+                    (cond (= x1 x2) (recur (rest set1) (rest set2) (cons x1 result))
+                          (< x1 x2) (recur (rest set1) set2 (cons x1 result))
+                          (> x1 x2) (recur set1 (rest set2) (cons x2 result))))))))
+
+(defn ol-make-set[& args]
+  (sort args))
+
+(defn ol-set=[set1 set2]
+  (= set1 set2))
+
 ;sets as unorderered lists with repeats
 (defn ul-repeat-element-of-set?[x a-set]
   (loop [x x
--- test/sicp/test/chapter_2/sets.clj
+++ test/sicp/test/chapter_2/sets.clj
@@ -5,25 +5,36 @@
 (deftest element-of-set-true
   (are [element-of-set? make-set] (element-of-set? 2 (make-set 4 6 2))
        ul-element-of-set? ul-make-set
+       ol-element-of-set? ol-make-set
        ul-repeat-element-of-set? ul-repeat-make-set))
 
 (deftest element-of-set-false
   (are [element-of-set? make-set] (not (element-of-set? 2 (make-set 4 6 3)))
        ul-element-of-set? ul-make-set
+       ol-element-of-set? ol-make-set
        ul-repeat-element-of-set? ul-repeat-make-set))
 
-(deftest adjoin
+(deftest adjoin-adds
   (are [set= adjoin-set make-set] (set= (adjoin-set 1 (make-set 2 3 4)) (make-set 1 2 3 4))
       ul-set= ul-adjoin-set ul-make-set
+      ol-set= ol-adjoin-set ol-make-set
+      ul-repeat-set= ul-repeat-adjoin-set ul-make-set))
+
+(deftest adjoin-ignores
+  (are [set= adjoin-set make-set] (set= (adjoin-set 1 (make-set 1 2 3 4)) (make-set 1 2 3 4))
+      ul-set= ul-adjoin-set ul-make-set
+      ol-set= ol-adjoin-set ol-make-set
       ul-repeat-set= ul-repeat-adjoin-set ul-make-set))
 
 (deftest intersection
   (are [set= intersection-set make-set] (set= (intersection-set (make-set 2 3 4) (make-set 5 4 3)) (make-set 3 4))
        ul-set= ul-intersection-set ul-make-set
+       ol-set= ol-intersection-set ol-make-set
        ul-repeat-set= ul-repeat-intersection-set ul-repeat-make-set))
 
 
 (deftest union
   (are [set= union-set make-set] (set= (union-set (make-set 1 5 4) (make-set  3 2 4 6)) (apply make-set (range 1 7)))
        ul-set= ul-union-set ul-make-set
+       ol-set= ol-union-set ol-make-set
        ul-repeat-set= ul-repeat-union-set ul-repeat-make-set))
