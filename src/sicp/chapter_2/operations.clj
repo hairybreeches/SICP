527dfaea5afe5d6e3cff61f167fd00906d38dac9
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2015-08-13 14:48:24 +0100

    exercise 2.75 - complex polar numbers in message-passing style

--- src/sicp/chapter_2/operations.clj
+++ src/sicp/chapter_2/operations.clj
@@ -1,4 +1,5 @@
-(ns sicp.chapter-2.operations)
+(ns sicp.chapter-2.operations
+  (:use clojure.math.numeric-tower))
 
 
 (defmulti get-record (fn [records id] (records :record-lookup)))
@@ -14,3 +15,21 @@
 
 (defn find-employee[id files]
   (first (filter #(not (nil? %)) (map #(get-record % id) files))))
+
+(defn make-from-real-imag[x y]
+  (defn dispatch[op]
+    (cond (= op 'real-part) x
+          (= op 'imag-part) y
+          (= op 'magnitude) (sqrt (+ (expt x 2) (expt y 2)))
+          (= op 'angle) (java.lang.Math/atan (/ y x))
+          :else (throw (Exception. (str "Unknown operator " op " make-from-real-imag"))))))
+
+(defn make-from-polar[magnitude angle]
+  (defn dispatch[op]
+    (cond (= op 'real-part) (* magnitude (java.lang.Math/cos angle))
+          (= op 'imag-part) (* magnitude (java.lang.Math/sin angle))
+          (= op 'magnitude) magnitude
+          (= op 'angle) angle
+          :else (throw (Exception. (str "Unknown operator " op " make-from-real-imag"))))))
+
+(defn apply-generic[op arg] (arg op))
