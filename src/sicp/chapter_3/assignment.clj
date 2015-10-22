6c2997033bbcee5272f7327052364705a5b0f07a
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2015-10-22 10:26:28 +0100

    exercise 3.2, counting function calls

--- src/sicp/chapter_3/assignment.clj
+++ src/sicp/chapter_3/assignment.clj
@@ -1,10 +1,24 @@
 (ns sicp.chapter-3.assignment)
 
 
-(defn make-accumulator[initial-value]
+(defn make-accumulator
+  [initial-value]
   (let [accumulate-value (ref initial-value)]
     (fn
       [to-add]
       (dosync
         (alter accumulate-value + to-add)
         @accumulate-value))))
+
+(defn make-monitored
+  [to-monitor]
+  (let [number-of-calls (ref 0)]
+    (fn
+      [& args]
+      (cond
+         (= (first args) :how-many-calls) @number-of-calls
+         (= (first args) :reset-count) (dosync (ref-set number-of-calls 0))
+         :else (dosync (alter number-of-calls inc)
+                       (apply to-monitor args))))))
+
+
--- test/sicp/test/chapter_3/assignment.clj
+++ test/sicp/test/chapter_3/assignment.clj
@@ -7,3 +7,13 @@
     (do (accumulator 13)
         (accumulator 14))
     (is (= (accumulator 11) 50))))
+
+
+(deftest can-monitor-calls
+  (let [monitored-square (make-monitored (fn[x] (* x x)))]
+    (is (= (monitored-square 2) 4))
+    (is (= (monitored-square 6) 36))
+    (is (= (monitored-square -9) 81))
+    (is (= (monitored-square :how-many-calls) 3))
+    (monitored-square :reset-count)
+    (is (= (monitored-square :how-many-calls) 0))))
