9b2e4d207c9555319bc0a6b9e30a3db3aaaf2854
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2015-10-22 11:58:34 +0100

    remove namespace clash

--- src/sicp/chapter_1/ex_16.clj
+++ src/sicp/chapter_1/ex_16.clj
@@ -1,6 +1,6 @@
 (ns sicp.chapter-1.ex-16)
 
-(defn even? [n]
+(defn is-even? [n]
     (= 0 (rem n 2)))
 
 (defn square [n]
@@ -11,7 +11,7 @@
          b b-initial
          n n-initial]
     (cond (= n 0) a
-        (even? n) (recur a (square b) (/ n 2))
+        (is-even? n) (recur a (square b) (/ n 2))
         :else (recur (* a b) b (- n 1)))))
 
 (defn expt [b n]
