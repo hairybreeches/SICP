c0eff68db485350a744f8ea7e99a4e572fb892c0
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2015-10-30 16:19:59 +0000

    3.18/19 - tests for a cycle finding function

--- test/sicp/test/chapter_3/lists.clj
+++ test/sicp/test/chapter_3/lists.clj
@@ -4,16 +4,9 @@
   (:use clojure.test))
 
 
-(def pair-list (let [pair-3 (cons-pair 3 nil)
-                     pair-2 (cons-pair 2 pair-3)
-                     pair-1 (cons-pair 1 pair-2)]
-                 pair-1))
+(def pair-list (make-list 3))
 
-(def pair-cycle (let [pair-3 (cons-pair 3 nil)
-                      pair-2 (cons-pair 2 pair-3)
-                      pair-1 (cons-pair 1 pair-2)]
-                     (set-cdr! pair-3 pair-1)
-             pair-1))
+(def pair-cycle (make-cycle 3))
 
 (def repeated-pair-list (let [pair-3 (cons-pair 3 nil)
                               pair-2 (cons-pair 2 pair-3)
@@ -44,3 +37,32 @@
 (deftest count-cycle
   (is (= (count-pairs pair-cycle) 3)))
 
+(deftest finds-cycle-odd
+  (is (contains-cycle? (make-cycle 7))))
+
+(deftest finds-cycle-even
+  (is (contains-cycle? (make-cycle 6))))
+
+(deftest finds-cycle-one-element
+  (is (contains-cycle? (make-cycle 1))))
+
+(deftest finds-cycle-two-element
+  (is (contains-cycle? (make-cycle 2))))
+
+(deftest finds-no-cycle-in-list
+  (is (not (contains-cycle? (make-list 12)))))
+
+(deftest finds-no-cycle-in-single-element-list
+  (is (not (contains-cycle? (make-list 1)))))
+
+(deftest finds-no-cycle-in-two-element-list
+  (is (not (contains-cycle? (make-list 2)))))
+
+(deftest finds-no-cycle-in-three-element-list
+  (is (not (contains-cycle? (make-list 3)))))
+
+(deftest finds-even-cycle-with-tail
+    (is (contains-cycle? (make-cycle-with-tail 8 12))))
+
+(deftest finds-odd-cycle-with-tail
+    (is (contains-cycle? (make-cycle-with-tail 8 15))))
