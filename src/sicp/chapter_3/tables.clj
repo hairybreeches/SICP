75f37fec644d205f8db4e5732976e93525783e9e
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2015-11-05 12:41:09 +0000

    exercise 3.25 - multikey tables

--- src/sicp/chapter_3/tables.clj
+++ src/sicp/chapter_3/tables.clj
@@ -12,31 +12,88 @@
 
 (defn make-table
   ([same-key?]
-  (let
-    [local-table (cons-pair :table nil)]
+    (let
+      [local-table (cons-pair :table nil)
 
-    (defn find-local-record
-      [kee]
-      (find-record kee (cdr local-table) same-key?))
+       find-local-record
+       (fn
+         [kee]
+         (find-record kee (cdr local-table) same-key?))
 
-    (defn lookup
-      [kee]
-      (let [record (find-local-record kee)]
-        (if record
-            (cdr record)
-            false)))
-
-    (defn insert!
-      [kee value]
-        (let [record (find-local-record kee)]
+       lookup
+       (fn
+          [kee]
+          (let [record (find-local-record kee)]
             (if record
-                (set-cdr! record value)
-                (set-cdr! local-table (cons-pair (cons-pair kee value) (cdr local-table))))))
+                (cdr record)
+                false)))
 
-    (defn dispatch
-      [m]
-      (cond (= m :lookup) lookup
-            (= m :insert!) insert!))))
+       insert!
+       (fn [value kee]
+           (let [record (find-local-record kee)]
+                (if record
+                    (set-cdr! record value)
+                    (set-cdr! local-table (cons-pair (cons-pair kee value) (cdr local-table))))))
+
+       dispatch
+       (fn [m]
+           (cond (= m :lookup) lookup
+                 (= m :insert!) insert!))]
+
+    dispatch))
+
      ([]
       (make-table =)))
+
+
+(defn make-multi-table
+  [same-key? depth]
+  (if (= depth 1)
+      (make-table same-key?)
+      (let [table (make-table same-key?)
+
+            lookup-record
+            (fn [kee]
+              ((table :lookup) kee))
+
+            inner-lookup
+            (fn [record kees]
+                (apply (record :lookup) kees))
+
+            inner-insert!
+            (fn [record value kees]
+                (apply (record :insert!) value kees))
+
+            create-record
+            (fn [kee]
+                (let [record (make-multi-table same-key? (dec depth))]
+                      ((table :insert!) record kee)
+                      record))
+
+            get-or-create-record!
+            (fn [kee]
+                (let [current (lookup-record kee)]
+                    (if current
+                        current
+                        (create-record kee))))
+
+            lookup
+            (fn [& kees]
+                (let [record (lookup-record (first kees))]
+                     (if record
+                         (inner-lookup record (rest kees))
+                        false)))
+
+            insert!
+            (fn [value & kees]
+                (let [record (get-or-create-record! (first kees))]
+                     (inner-insert! record value (rest kees))))
+
+            dispatch
+            (fn [m]
+                (cond (= m :lookup) lookup
+                      (= m :insert!) insert!))
+            ]
+
+        dispatch)))
 
