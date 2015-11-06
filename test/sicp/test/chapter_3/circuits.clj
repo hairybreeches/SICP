61292e258c3b6b28f8647dce882a2f20553496db
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2015-11-06 16:54:22 +0000

    check the other case

--- test/sicp/test/chapter_3/circuits.clj
+++ test/sicp/test/chapter_3/circuits.clj
@@ -3,12 +3,27 @@
   (:use sicp.chapter-3.circuits))
 
 
-(deftest an-inverter-inverts-its-initial-current-when-its-zero
-  (let [wire-in (make-wire 0)
+(defn test-inverter
+  [input-value ultimate-output]
+  (let [wire-in (make-wire input-value)
         wire-out (make-wire)
         inverter (inverter wire-in wire-out)]
+
+    ;the base state of the wire is 0
     (is (= (get-signal wire-out) 0))
+
     (increment-time!)
+    ;after one time, the inverter has not worked yet (it has a delay of two)
     (is (= (get-signal wire-out) 0))
+
     (increment-time!)
-    (is (= (get-signal wire-out) 1))))
+    ;after two time, the inverter has worked, and the signal should be the inverse of the input
+    (is (= (get-signal wire-out) ultimate-output))))
+
+
+
+(deftest an-inverter-inverts-its-initial-current-when-its-zero
+  (test-inverter 1 0))
+
+(deftest an-inverter-inverts-its-initial-current-when-its-one
+  (test-inverter 0 1))
