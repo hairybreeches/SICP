b395ce242c1687a299f2117a8a29a11765fa1abe
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2016-01-08 14:20:02 +0000

    make values mutable and implement set-variable-value!

--- src/sicp/chapter_4/environments.clj
+++ src/sicp/chapter_4/environments.clj
@@ -1,14 +1,21 @@
 (ns sicp.chapter-4.environments
   (:use sicp.error))
 
+(defn- make-value
+  [value]
+  (ref value))
+
+(defn- retrieve-value
+  [value]
+  @value)
+
 (defn- make-frame-value
   [variables values]
   (list variables values))
 
 (defn- make-frame
   [variables values]
-  (ref (make-frame-value variables values)))
-
+  (ref (make-frame-value variables (map make-value values))))
 
 (defn- frame-variables
   [frame]
@@ -22,7 +29,7 @@
   [frame var value]
   (make-frame-value
     (cons var (frame-variables frame))
-    (cons value (frame-values frame))))
+    (cons (make-value value) (frame-values frame))))
 
 (defn- add-binding-to-frame!
   [var value frame]
@@ -44,7 +51,7 @@
   (letfn [(env-loop [env]
                     (letfn [(scan [vars values]
                                   (cond (empty? vars) (env-loop (enclosing-environment env))
-                                        (= var (first vars)) (first values)
+                                        (= var (first vars)) (retrieve-value (first values))
                                         :else (scan (rest vars) (rest values))))]
 
                     (if (= env the-empty-environment)
@@ -64,7 +71,19 @@
 
 
 (defn define-variable!
-  [var value env])
+  [var value env]
+  )
 
 (defn set-variable-value!
-  [var value env])
+  [var value env]
+  (letfn [(env-loop [env]
+                    (letfn [(scan [vars values]
+                                  (cond (empty? vars) (env-loop (enclosing-environment env))
+                                        (= var (first vars)) (dosync (ref-set (first values) value))
+                                        :else (scan (rest vars) (rest values))))]
+                      (if (= env the-empty-environment)
+                          (error "Unbound variable:" var)
+                          (let [frame (first-frame env)]
+                            (scan (frame-variables frame)
+                                  (frame-values frame))))))]
+    (env-loop env)))
--- test/sicp/test/chapter_4/environments.clj
+++ test/sicp/test/chapter_4/environments.clj
@@ -7,3 +7,8 @@
          (lookup-variable-value
            'x
            (extend-environment '(x) '(4) the-empty-environment)))))
+
+(deftest can-set-existing-value
+  (let [env (extend-environment '(x) '(12) the-empty-environment)]
+    (set-variable-value! 'x 5 env)
+    (is (= (lookup-variable-value 'x env) 5))))
