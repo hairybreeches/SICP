e462ca6e825bc5db775187d7a3091c5069fe807b
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2016-01-08 12:21:55 +0000

    implementation of frames

--- src/sicp/chapter_4/environments.clj
+++ src/sicp/chapter_4/environments.clj
@@ -1,5 +1,33 @@
 (ns sicp.chapter-4.environments)
 
+(defn- make-frame-value
+  [variables values]
+  (list variables values))
+
+(defn- make-frame
+  [variables values]
+  (ref (make-frame-value variables values)))
+
+
+(defn- frame-variables
+  [frame]
+  (first @frame))
+
+(defn- frame-values
+  [frame]
+  (second @frame))
+
+(defn- append-to-frame
+  [frame var value]
+  (make-frame-value
+    (cons var (frame-variables frame))
+    (cons value (frame-values frame))))
+
+(defn- add-binding-to-frame!
+  [var value frame]
+  (alter frame (fn [fr] (append-to-frame fr var value))))
+
+
 (defn- enclosing-environment
   [env]
   (rest env))
