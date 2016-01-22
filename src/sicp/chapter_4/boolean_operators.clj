baf7ab10a13ff9d2d3e803f599aa304f9a11eeea
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2016-01-22 18:05:37 +0000

    boolean operators

--- src/sicp/chapter_4/boolean_operators.clj
+++ src/sicp/chapter_4/boolean_operators.clj
@@ -10,21 +10,21 @@
                false)))
 
 (defn- make-not [t]
-  (list 'not t))
+  (create-expression 'not (list t)))
 
 (defn make-and
   [tests]
-  (cons 'and tests))
+  (create-expression 'and tests))
 
 (defmethod my-eval 'not [exp env]
-  (my-eval (make-if (second exp) false true) env))
+  (my-eval (make-if (first (operands exp)) false true) env))
 
 (defmethod my-eval 'and [exp env]
-  (my-eval (and->if (rest exp)) env))
+  (my-eval (and->if (operands exp)) env))
 
 (defmethod my-eval 'or [exp env]
   (my-eval
     (make-not
       (make-and
-        (map make-not (rest exp))))
+        (map make-not (operands exp))))
     env))
