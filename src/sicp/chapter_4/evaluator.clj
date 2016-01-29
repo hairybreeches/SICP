c378c6da326d14e239cf4689b00cda91a3426514
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2016-01-29 15:56:51 +0000

    reorder functions

--- src/sicp/chapter_4/evaluator.clj
+++ src/sicp/chapter_4/evaluator.clj
@@ -1,6 +1,7 @@
 (ns sicp.chapter-4.evaluator
   (:use sicp.error))
 
+;syntax
 (defn operator
   [exp]
   (first exp))
@@ -13,14 +14,22 @@
   [form operands]
   (cons form operands))
 
+;dispatch functions
 (defn get-exp-type
   [exp env]
   (if (seq? exp)
       (operator exp)
       (type exp)))
 
-(defmulti my-eval get-exp-type)
+(defn- get-procedure-type
+  [procedure arguments]
+  (first procedure))
 
+;eval/apply
+(defmulti my-eval get-exp-type)
+(defmulti my-apply get-procedure-type)
+
+;evaluating sequences
 (defn first-exp
   [exp]
   (first exp))
@@ -41,9 +50,3 @@
         (my-eval (first-exp exps) env)
         (do (my-eval (first-exp exps) env)
             (recur (rest-exps exps) env)))))
-
-(defn- get-procedure-type
-  [procedure arguments]
-  (first procedure))
-
-(defmulti my-apply get-procedure-type)
