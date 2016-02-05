99e3ec9225a5a541715488104b497d68c3a1e838
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2016-02-05 18:08:12 +0000

    exercise 4.21 - the y combinator

--- test/sicp/test/chapter_4/evaluator.clj
+++ test/sicp/test/chapter_4/evaluator.clj
@@ -245,4 +245,29 @@
              (odd? 4))))
 
 
+(deftest factorial-y-combinator
+  (evals-to 720
+  '((lambda (n) ((lambda (fact) (fact fact n))
+                         (lambda (ft k)
+                                 (if (= k 1)
+                                   1
+                                   (* k (ft ft (- k 1)))))))
+    6)
+            ))
+
+(deftest odd-even-y-combinator
+  (evals-to false
+          '(begin
+            (define (f x)
+               ((lambda (even? odd?)
+                        (even? even? odd? x))
+                (lambda (ev? od? n)
+                        (if (= n 0) true (od? ev? od? (- n 1))))
+                (lambda (ev? od? n)
+                        (if (= n 0) false (ev? ev? od? (- n 1))))))
+
+             (f 11))
+            ))
+
+
 
