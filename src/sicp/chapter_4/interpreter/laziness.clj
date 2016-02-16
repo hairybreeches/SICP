07c15ad991932669b8d9e2e1d71326a8d11d2fae
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2016-02-16 15:54:49 +0000

    memoisation (from the book)

--- src/sicp/chapter_4/interpreter/laziness.clj
+++ src/sicp/chapter_4/interpreter/laziness.clj
@@ -1,32 +1,57 @@
 (ns sicp.chapter-4.interpreter.laziness
   (:use sicp.chapter-4.interpreter.evaluator))
 
+(def actual-value)
 
+(defn- referenced-list?
+  [exp tag]
+  (and (= (type exp) clojure.lang.Ref)
+       (seq? @exp)
+       (= (first @exp) tag)))
+
+;thunks
 (defn- make-thunk
   [exp env]
-  (list 'thunk exp env))
+  (ref
+    (list 'thunk exp env)))
 
 (defn- thunk?
   [exp]
-  (and (seq? exp)
-       (= (first exp)
-          'thunk)))
+  (referenced-list? exp 'thunk))
 
-(defn thunk-exp
+(defn thunk-exp-from-value
   [thunk]
   (second thunk))
 
-(defn thunk-env
+(defn thunk-env-from-value
   [thunk]
   (nth thunk 2))
 
-(def actual-value)
+;evaluated thunks
+(defn- evaluated-thunk?
+  [exp]
+  (referenced-list? exp 'evaluated-thunk))
 
+(defn- thunk-value
+  [evaluated-thunk]
+  (second @evaluated-thunk))
+
+(defn- evaluate-thunk
+  [thunk]
+  (list 'evaluated-thunk (actual-value (thunk-exp-from-value thunk)
+                                       (thunk-env-from-value thunk))))
+
+(defn- process-thunk
+  [thunk]
+  (dosync
+    (alter thunk evaluate-thunk)))
+
+;laziness
 (defn- force-it
   [obj]
-  (if (thunk? obj)
-    (actual-value (thunk-exp obj) (thunk-env obj))
-    obj))
+  (cond (thunk? obj) (do (process-thunk obj) (thunk-value obj))
+        (evaluated-thunk? obj) (thunk-value obj)
+        :else obj))
 
 (defn delay-it
   [exp env]
