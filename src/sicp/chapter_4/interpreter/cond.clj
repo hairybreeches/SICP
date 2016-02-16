08092082a68842ea29309ef77c8b1fbad0ddfd74
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2016-02-16 13:16:40 +0000

    rewrite cond to work more like the original, and not mix eval and parsing

--- src/sicp/chapter_4/interpreter/cond.clj
+++ src/sicp/chapter_4/interpreter/cond.clj
@@ -1,12 +1,14 @@
 (ns sicp.chapter-4.interpreter.cond
   (:use sicp.chapter-4.interpreter.evaluator)
-  (:use sicp.chapter-4.interpreter.primitive-datatypes)
+  (:use sicp.chapter-4.interpreter.if)
+  (:use sicp.chapter-4.interpreter.let)
   (:use sicp.chapter-4.interpreter.begin)
+  (:use sicp.chapter-4.interpreter.lambda)
   (:use sicp.error))
 
 (defn- cond-clauses
   [exp]
-  (operands exp))
+  (rest exp))
 
 (defn- cond-predicate
   [clause]
@@ -28,24 +30,43 @@
   [clause]
   (nth clause 2))
 
-(defn- eval-cond
-  [clauses env]
-  (loop [clauses clauses]
+(defn- expand-clauses
+  [clauses]
   (if
     (empty? clauses)
     false
     (let [first-clause (first clauses)
           rest-clauses (rest clauses)]
-      (if (cond-else-clause? first-clause)
-          (if (empty? rest-clauses)
-              (sequence->exp (cond-actions first-clause))
-              (error "else clause not last: " clauses))
-          (let [predicate-result (my-eval (cond-predicate first-clause) env)]
-               (if (my-true? predicate-result)
-                   (if (function-clause? first-clause)
-                       (my-apply (my-eval (clause-function first-clause) env) (list predicate-result))
-                       (my-eval (sequence->exp (cond-actions first-clause)) env))
-                   (recur rest-clauses))))))))
+      (cond
+
+        (cond-else-clause? first-clause)
+        (if (empty? rest-clauses)
+            (sequence->exp (cond-actions first-clause))
+            (error "else clause not last: " clauses))
+
+        (function-clause? first-clause)
+        (let [result-name (gensym)]
+          (make-let
+            (list
+              (list
+                result-name
+                (cond-predicate first-clause)))
+            (make-if
+              result-name
+              (create-expression
+                  (clause-function first-clause)
+                  (list result-name))
+
+              (expand-clauses rest-clauses))))
+
+        :else
+        (make-if (cond-predicate first-clause)
+                 (sequence->exp (cond-actions first-clause))
+                 (expand-clauses rest-clauses))))))
+
+(defn cond->if
+  [exp]
+  (expand-clauses (cond-clauses exp)))
 
 (defmethod my-eval 'cond [exp env]
-  (eval-cond (cond-clauses exp) env))
+  (my-eval (cond->if exp) env))
