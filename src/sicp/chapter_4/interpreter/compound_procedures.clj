7df338d7206f9cf1f032149233a9ad6445324bb0
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2016-02-16 14:12:34 +0000

    Merge with default

--- src/sicp/chapter_4/interpreter/compound_procedures.clj
+++ src/sicp/chapter_4/interpreter/compound_procedures.clj
@@ -1,59 +1,9 @@
 (ns sicp.chapter-4.interpreter.compound-procedures
+  (:use sicp.chapter-4.interpreter.variable-hoisting)
   (:use sicp.chapter-4.interpreter.evaluator)
-  (:use sicp.chapter-4.interpreter.begin)
-  (:use sicp.chapter-4.interpreter.environments)
   (:use sicp.chapter-4.interpreter.lambda)
-  (:use sicp.chapter-4.interpreter.definition)
-  (:use sicp.chapter-4.interpreter.assignment))
+  (:use sicp.chapter-4.interpreter.environments))
 
-;hoisting variables
-(defn- hoist-variables
-  [variable-names parsed-statement]
-  (sequence->exp
-     (concat
-        (map make-define variable-names (repeat '(quote *unassigned*)))
-        (list parsed-statement))))
-
-(defn- identity-hoist
-  [exp]
-  {:variable-names '() :statement exp})
-
-(defmulti parse-defines get-exp-type)
-
-(defmethod parse-defines 'lambda [exp] (identity-hoist exp))
-(defmethod parse-defines java.lang.Boolean [exp] (identity-hoist exp))
-(defmethod parse-defines java.lang.Number [exp] (identity-hoist exp))
-(defmethod parse-defines java.lang.String [exp] (identity-hoist exp))
-(defmethod parse-defines clojure.lang.Symbol [exp] (identity-hoist exp))
-(defmethod parse-defines 'define [exp]
-  {
-    :variable-names (list (definition-variable exp))
-    :statement (make-set (definition-variable exp) (definition-value exp))
-  })
-
-(defn- combine
-  [summary-so-far current]
-  {
-    :variable-names (concat (:variable-names summary-so-far)
-                           (:variable-names current))
-    :statement (conj (:statement summary-so-far) (:statement current))
-  })
-
-(defmethod parse-defines :default
-  [exp]
-  (let [result (reduce
-                  combine
-                  {:variable-names '() :statement []}
-                  (map parse-defines exp))]
-    {:variable-names (:variable-names result)
-     :statement (apply list (:statement result))}))
-
-(defn- scan-out-defines
-  [exp]
-  (let [result (parse-defines exp)]
-    (hoist-variables (:variable-names result) (:statement result))))
-
-;compound procedures
 (defn- tagged-list?
   [exp tag]
   (if (seq? exp)
@@ -89,6 +39,6 @@
 
 (defmethod analyse 'lambda [exp]
   (let [vars (lambda-parameters exp)
-        body (analyse (scan-out-defines (sequence->exp (lambda-body exp))))]
+        body (analyse (hoist-variables (sequence->exp (lambda-body exp))))]
     (fn [env]
       (make-procedure vars body env))))
