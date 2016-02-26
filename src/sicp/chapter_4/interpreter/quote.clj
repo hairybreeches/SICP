dc7ab2d26aea776600eb1b641a90f7291b9a9a54
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2016-02-26 18:12:02 +0000

    exercise 4.33 - make quoted lists turn into lazy lists

--- src/sicp/chapter_4/interpreter/quote.clj
+++ src/sicp/chapter_4/interpreter/quote.clj
@@ -5,5 +5,23 @@
   [exp]
   (first (operands exp)))
 
+(defn list->lazy-list
+  [l]
+  (if (empty? l)
+      'null
+      (create-expression
+        'cons
+        (list
+          (create-expression 'quote (list (first l)))
+          (list->lazy-list (rest l))))))
+
+(defn to-lazy-lists
+  [exp env]
+  (cond (not (seq? exp)) exp
+        :else (-> (map #(to-lazy-lists % env) exp)
+                  (list->lazy-list)
+                  (my-eval env))))
+
 (defmethod my-eval 'quote [exp env]
-  (text-of-quotation exp))
+  (let [result (to-lazy-lists (text-of-quotation exp) env)]
+  result))
