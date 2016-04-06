7eeed4f0f14b08b1a206bc53743e12294b7d6070
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2016-04-06 17:57:38 +0100

    code from the book to solve the multiple dwelling problem

--- test/sicp/test/chapter_4/amb.clj
+++ test/sicp/test/chapter_4/amb.clj
@@ -67,3 +67,30 @@
 
       '((3 4 5) (6 8 10) (5 12 13) (9 12 15)))))
 
+(deftest multiple-dwelling
+  (is (=
+        (get-all-results
+          require-code
+          '(let ((baker (amb 1 2 3 4 5))
+                 (cooper (amb 1 2 3 4 5))
+                 (fletcher (amb 1 2 3 4 5))
+                 (miller (amb 1 2 3 4 5))
+                 (smith (amb 1 2 3 4 5)))
+             (require (distinct? baker cooper fletcher miller smith))
+             (require (not (= baker 5)))
+             (require (not (= cooper 1)))
+             (require (not (= fletcher 5)))
+             (require (not (= fletcher 1)))
+             (require (> miller cooper))
+             (require (not (= (abs (- smith fletcher)) 1)))
+             (require (not (= (abs (- fletcher cooper)) 1)))
+             (list (list 'baker baker)
+                   (list 'cooper cooper)
+                   (list 'fletcher fletcher)
+                   (list 'miller miller)
+                   (list 'smith smith))))
+        '(((baker 3) (cooper 2) (fletcher 4) (miller 5) (smith 1)))
+        )))
+
+
+
