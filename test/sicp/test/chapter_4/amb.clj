2132241dac0394536667f8623f17165cf11cbac0
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2016-04-07 14:13:38 +0100

    move clojure code into separate file

--- test/sicp/test/chapter_4/amb.clj
+++ test/sicp/test/chapter_4/amb.clj
@@ -132,58 +132,6 @@
         '(((baker 3) (cooper 2) (fletcher 4) (miller 5) (smith 1)))
         )))
 
-(defn exclude
-  [options & to-exclude]
-  (let [exclude-set (apply hash-set to-exclude)]
-   (filter
-     #(not (exclude-set %))
-     options)))
-
-(defn baker-options []
-  '(1 2 3 4))
-
-(defn cooper-options [so-far]
-  (let [baker (:baker so-far)]
-    (exclude '(2 3 4 5) baker)))
-
-(defn fletcher-options [so-far]
-  (let [baker (:baker so-far)
-        cooper (:cooper so-far)]
-  (exclude '(2 3 4) baker cooper (+ cooper 1) (- cooper 1))))
-
-(defn miller-options [so-far]
-  (let [baker (:baker so-far)
-        cooper (:cooper so-far)
-        fletcher (:fletcher so-far)]
-  (filter
-    #(> % cooper)
-    (exclude '(1 2 3 4 5) baker cooper fletcher))))
-
-(defn smith-options [so-far]
-  (let [baker (:baker so-far)
-        cooper (:cooper so-far)
-        fletcher (:fletcher so-far)
-        miller (:miller so-far)]
-  (exclude '(1 2 3 4 5) baker cooper fletcher miller (+ fletcher 1) (- fletcher 1))))
-
-(defn add-options [so-far n options]
-  (map #(assoc so-far n %) options))
-
-(defn refine-options [generator n options-so-far]
-  (mapcat #(add-options % n (generator %)) options-so-far))
-
-(defn multiple-dwelling []
-  (->>
-    (baker-options)
-    (map (fn [%] {:baker %}))
-    (refine-options cooper-options :cooper)
-    (refine-options fletcher-options :fletcher)
-    (refine-options miller-options :miller)
-    (refine-options smith-options :smith)))
-
-(deftest multiple-dwelling-clojure
-  (is (= (multiple-dwelling) '({:baker 3 :cooper 2 :fletcher 4 :miller 5 :smith 1}))))
-
 (deftest liars
   (is (=
         (get-all-results
