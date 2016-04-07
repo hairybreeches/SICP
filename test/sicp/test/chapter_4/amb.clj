1eb9b70d257f74409608bfbf246cf60e476d5598
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2016-04-07 13:10:04 +0100

    exercise 4.42 - liars

--- test/sicp/test/chapter_4/amb.clj
+++ test/sicp/test/chapter_4/amb.clj
@@ -180,6 +180,29 @@
 (deftest multiple-dwelling-clojure
   (is (= (multiple-dwelling) '({:baker 3 :cooper 2 :fletcher 4 :miller 5 :smith 1}))))
 
+(deftest liars
+  (is (=
+        (get-all-results
+          require-code
+          an-element-of
+          '(let ((betty (amb 1 2 3 4 5))
+                 (ethel (amb 1 2 3 4 5))
+                 (joan (amb 1 2 3 4 5))
+                 (kitty (amb 1 2 3 4 5))
+                 (mary (amb 1 2 3 4 5)))
+             (require (distinct? betty ethel joan kitty mary))
+             (require (xor (= kitty 2) (= betty 3)))
+             (require (xor (= ethel 1) (= joan 2)))
+             (require (xor (= joan 3) (= ethel 5)))
+             (require (xor (= kitty 2) (= mary 4)))
+             (require (xor (= mary 4) (= betty 1)))
+             (list (list 'betty betty)
+                   (list 'ethel ethel)
+                   (list 'joan joan)
+                   (list 'kitty kitty)
+                   (list 'mary mary))))
+        '(((betty 3) (ethel 5) (joan 2) (kitty 1) (mary 4))))))
+
 
 
 
