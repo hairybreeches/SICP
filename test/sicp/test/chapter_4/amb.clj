e08ad93941b0a1d41f6e0c1dd045ebb26dc0692e
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2016-04-07 10:34:38 +0100

    exercise 3.40 - remove easiest restrictions to simplify

--- test/sicp/test/chapter_4/amb.clj
+++ test/sicp/test/chapter_4/amb.clj
@@ -71,16 +71,12 @@
   (is (=
         (get-all-results
           require-code
-          '(let ((baker (amb 1 2 3 4 5))
-                 (cooper (amb 1 2 3 4 5))
-                 (fletcher (amb 1 2 3 4 5))
+          '(let ((baker (amb 1 2 3 4))
+                 (cooper (amb 2 3 4 5))
+                 (fletcher (amb 2 3 4))
                  (miller (amb 1 2 3 4 5))
                  (smith (amb 1 2 3 4 5)))
              (require (distinct? baker cooper fletcher miller smith))
-             (require (not (= baker 5)))
-             (require (not (= cooper 1)))
-             (require (not (= fletcher 5)))
-             (require (not (= fletcher 1)))
              (require (> miller cooper))
              (require (not (= (abs (- smith fletcher)) 1)))
              (require (not (= (abs (- fletcher cooper)) 1)))
