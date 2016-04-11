69c29b8aa4f8e385257d56c3dbbd0d4723e3d78e
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2016-04-11 18:17:28 +0100

    code from the book to parse more complex sentences, incl ex 4.45

--- src/sicp/chapter_4/language.clj
+++ src/sicp/chapter_4/language.clj
@@ -13,6 +13,8 @@
 
     '(define articles '(article the a))
 
+    '(define prepositions '(prep for to in by with))
+
     '(define (parse-word word-list)
       (require (not (null? *unparsed*)))
       (require (member? (cdr word-list) (car *unparsed*)))
@@ -20,15 +22,40 @@
         (set! *unparsed* (cdr *unparsed*))
         (list (car word-list) found-word)))
 
-    '(define (parse-noun-phrase)
-      (list 'noun-phrase
+    '(define (parse-simple-noun-phrase)
+      (list 'simple-noun-phrase
             (parse-word articles)
             (parse-word nouns)))
+
+    '(define (parse-prepositional-phrase)
+       (list 'prep-phrase
+             (parse-word prepositions)
+             (parse-noun-phrase)))
+
+    '(define (parse-noun-phrase)
+       (define (maybe-extend noun-phrase)
+         (amb noun-phrase
+              (maybe-extend
+                (list 'noun-phrase
+                      noun-phrase
+                      (parse-prepositional-phrase)))))
+       (maybe-extend (parse-simple-noun-phrase)))
+
+    '(define (parse-verb-phrase)
+       (define (maybe-extend verb-phrase)
+         (amb verb-phrase
+              (maybe-extend
+                (list
+                  'verb-phrase
+                  verb-phrase
+                  (parse-prepositional-phrase)))))
+       (maybe-extend (parse-word verbs)))
 
     '(define (parse-sentence)
       (list 'sentence
             (parse-noun-phrase)
-            (parse-word verbs)))
+            (parse-verb-phrase)))
+
 
     '(define *unparsed* '())
 
--- test/sicp/test/chapter_4/amb.clj
+++ test/sicp/test/chapter_4/amb.clj
@@ -41,7 +41,65 @@
 
 (deftest simple-sentence
   (is (= (parse ''(the cat eats))
-         '((sentence (noun-phrase (article the) (noun cat)) (verb eats))))))
+         '((sentence (simple-noun-phrase (article the) (noun cat)) (verb eats))))))
+
+(deftest intermediate-sentence
+  (is (= (parse ''(the professor lectures to the student with the cat))
+         '(
+            ;the professor lectures with the cat
+            (sentence
+              (simple-noun-phrase (article the) (noun professor))
+              (verb-phrase
+                (verb-phrase (verb lectures) (prep-phrase (prep to) (simple-noun-phrase (article the) (noun student))))
+                (prep-phrase (prep with) (simple-noun-phrase (article the) (noun cat)))))
+
+            ;the student has the cat
+            (sentence
+              (simple-noun-phrase (article the) (noun professor))
+              (verb-phrase
+                (verb lectures)
+                (prep-phrase
+                  (prep to)
+                  (noun-phrase (simple-noun-phrase (article the) (noun student))
+                               (prep-phrase (prep with) (simple-noun-phrase (article the) (noun cat)))))))))))
+
+(deftest complex-sentence
+  (is (= (parse ''(the professor lectures to the student in the class with the cat))
+         '(
+            (sentence
+              (simple-noun-phrase (article the) (noun professor))
+              (verb-phrase
+                (verb-phrase
+                  (verb-phrase (verb lectures) (prep-phrase (prep to) (simple-noun-phrase (article the) (noun student))))
+                  (prep-phrase (prep in) (simple-noun-phrase (article the) (noun class))))
+                (prep-phrase (prep with) (simple-noun-phrase (article the) (noun cat)))))
+
+
+            (sentence
+              (simple-noun-phrase (article the) (noun professor))
+              (verb-phrase
+                (verb-phrase (verb lectures) (prep-phrase (prep to) (simple-noun-phrase (article the) (noun student))))
+                (prep-phrase (prep in) (noun-phrase (simple-noun-phrase (article the) (noun class))
+                                                    (prep-phrase (prep with) (simple-noun-phrase (article the) (noun cat)))))))
+
+            (sentence
+              (simple-noun-phrase (article the) (noun professor))
+              (verb-phrase
+                (verb-phrase (verb lectures) (prep-phrase (prep to) (noun-phrase (simple-noun-phrase (article the) (noun student))
+                                                                                 (prep-phrase (prep in) (simple-noun-phrase (article the) (noun class))))))
+                (prep-phrase (prep with) (simple-noun-phrase (article the) (noun cat)))))
+
+            (sentence
+              (simple-noun-phrase (article the) (noun professor))
+              (verb-phrase (verb lectures) (prep-phrase (prep to) (noun-phrase (noun-phrase (simple-noun-phrase (article the) (noun student))
+                                                                                            (prep-phrase (prep in) (simple-noun-phrase (article the) (noun class))))
+                                                                               (prep-phrase (prep with) (simple-noun-phrase (article the) (noun cat)))))))
+
+            (sentence
+              (simple-noun-phrase (article the) (noun professor))
+              (verb-phrase (verb lectures) (prep-phrase (prep to) (noun-phrase (simple-noun-phrase (article the) (noun student))
+                                                                               (prep-phrase (prep in) (noun-phrase (simple-noun-phrase (article the) (noun class))
+                                                                                                                   (prep-phrase (prep with) (simple-noun-phrase (article the) (noun cat)))))))))))))
 
 
 
