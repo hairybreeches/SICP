cc8e3ef53586e98624c4e41b4aaceee86f6466db
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2016-04-14 17:21:16 +0100

    and

---
+++ src/sicp/chapter_4/logic/and.clj
@@ -0,0 +1,14 @@
+(ns sicp.chapter-4.logic.and
+  (:use sicp.chapter-4.logic.evaluation))
+
+(defn- conjoin [conjuncts frames]
+  (loop [conjuncts frames]
+    (if (empty? conjuncts)
+        frames
+      (recur (rest conjuncts)
+             (qeval (first conjuncts)
+                    frames)))))
+
+(defmethod qeval :and [query-pattern frames]
+  (conjoin query-pattern frames))
+
