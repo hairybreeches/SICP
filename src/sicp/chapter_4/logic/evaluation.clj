b4e73416c6e0ad088c82118db807e5c324feb914
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2016-04-14 17:21:06 +0100

    we'll use type here

--- src/sicp/chapter_4/logic/evaluation.clj
+++ src/sicp/chapter_4/logic/evaluation.clj
@@ -1,6 +1,3 @@
 (ns sicp.chapter-4.logic.evaluation)
 
-(defn- expression-type [query]
-  )
-
-(defmulti qeval (fn [query frames] (expression-type query)))
+(defmulti qeval (fn [query frames] (type query)))
