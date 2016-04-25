d10d10147b225621ce52e1cc1611f82eb6787cd8
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2016-04-25 17:21:23 +0100

    give sample db its own file

--- test/sicp/test/chapter_4/logic.clj
+++ test/sicp/test/chapter_4/logic.clj
@@ -1,73 +1,7 @@
 (ns sicp.test.chapter-4.logic
+  (:use sicp.test.chapter-4.sample-database)
   (:use sicp.chapter-4.logic.querier)
   (:use clojure.test))
-
-(def people
-  '((address (Bitdiddle Ben) (Slumerville (Ridge Road) 10))
-    (job (Bitdiddle Ben) (computer wizard))
-    (salary (Bitdiddle Ben) 60000)
-    (supervisor (Bitdiddle Ben) (Warbucks Oliver))
-
-    (address (Hacker Alyssa P) (Cambridge (Mass Ave) 78))
-    (job (Hacker Alyssa P) (computer programmer))
-    (salary (Hacker Alyssa P) 40000)
-    (supervisor (Hacker Alyssa P) (Bitdiddle Ben))
-
-    (address (Fect Cy D) (Cambridge (Ames Street) 3))
-    (job (Fect Cy D) (computer programmer))
-    (salary (Fect Cy D) 35000)
-    (supervisor (Fect Cy D) (Bitdiddle Ben))
-
-    (address (Tweakit Lem E) (Boston (Bay State Road) 22))
-    (job (Tweakit Lem E) (computer technician))
-    (salary (Tweakit Lem E) 25000)
-    (supervisor (Tweakit Lem E) (Bitdiddle Ben))
-
-    (address (Reasoner Louis) (Slumerville (Pine Tree Road) 80))
-    (job (Reasoner Louis) (computer programmer trainee))
-    (salary (Reasoner Louis) 30000)
-    (supervisor (Reasoner Louis) (Hacker Alyssa P))
-
-    (address (Warbucks Oliver) (Swellesley (Top Heap Road)))
-    (job (Warbucks Oliver) (administration big wheel))
-    (salary (Warbucks Oliver) 150000)
-
-    (address (Scrooge Eben) (Weston (Shady Lane) 10))
-    (job (Scrooge Eben) (accounting chief accountant))
-    (salary (Scrooge Eben) 75000)
-    (supervisor (Scrooge Eben) (Warbucks Oliver))
-
-    (address (Cratchet Robert) (Allston (N Harvard Street) 16))
-    (job (Cratchet Robert) (accounting scrivener))
-    (salary (Cratchet Robert) 18000)
-    (supervisor (Cratchet Robert) (Scrooge Eben))
-
-    (address (Aull DeWitt) (Slumerville (Onion Square) 5))
-    (job (Aull DeWitt) (administration secretary))
-    (salary (Aull DeWitt) 25000)
-    (supervisor (Aull DeWitt) (Warbucks Oliver))
-
-    (can-do-job (computer wizard) (computer programmer))
-    (can-do-job (computer wizard) (computer technician))
-    (can-do-job (computer programmer) (computer programmer trainee))
-    (can-do-job (administration secretary) (administration big wheel))
-
-    (rule (same ?x ?x))
-
-    (rule (replaces ?person1 ?person2)
-          (and (job ?person1 ?person1-job)
-               (job ?person1 ?person2-job)
-               (or (same ?person1-job ?person2-job)
-                   (can-do-job ?person1-job ?person2-job))
-               (not (same ?person1 ?person2))))
-
-    (rule (big-shot ?person ?division)
-          (and (job ?person (?division . ?role))
-               (not (and (supervisor ?person ?supervisor)
-                         (job ?supervisor (?division . ?supervisor-role))))))
-
-    ))
-
 
 (deftest retrieve-by-supervisor
   (is (=
