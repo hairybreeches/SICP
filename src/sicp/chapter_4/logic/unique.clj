b2759551d05d3e0c371464688e64d40c876db44e
Dave Conlin <709166+hairybreeches@users.noreply.github.com>
2016-06-10 11:14:16 +0100

    implement unique - all tests now passing again

--- src/sicp/chapter_4/logic/unique.clj
+++ src/sicp/chapter_4/logic/unique.clj
@@ -6,8 +6,37 @@
   (:require [schema.core :as s]))
 
 
+;I wonder if there's a way to do this without state?
 (s/defn analyse-unique [unique-statement]
-        (error "not yet implemented unique"))
+        (let [unique-function (analyse unique-statement)]
+          (s/fn
+            [frame :- Frame
+             rule-stack :- Rule-Stack
+             success
+             fail]
+
+            (let [result (ref false)
+                  have-received-result (ref false)]
+
+              (unique-function
+                frame
+                rule-stack
+                (fn [this-result
+                     fail2]
+                  (if @have-received-result
+                    (fail) ;there was more than one result
+                    (dosync
+                      (ref-set result this-result)
+                      (ref-set have-received-result true)
+                      (fail2) ;see if there are any more results
+
+                      )))
+
+                (fn []
+                  (if @have-received-result
+                    (success @result fail) ;there was only one result
+                    (fail);there were no results
+                    )))))))
 
 (defmethod analyse-dispatch 'unique [_ query-pattern]
   (analyse-unique (first query-pattern)))
